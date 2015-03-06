7808c8fe8da65616831052607407eacf365a889f
benjamintomad <benjamin.tomad@gmail.com>
2015-03-06 12:05:40 +0100

    fix rotation happening too late

--- RTS/bakeTexAnimRotateLoc.py
+++ RTS/bakeTexAnimRotateLoc.py
@@ -4,9 +4,9 @@ import lx
 
 # set frame range and intialize counter
 firstFrame = 0
-lastFrame = 119
+lastFrame = 120
 counter = firstFrame
-counterRot = 0
+counterRot = firstFrame * 3
 
 # get all items
 n = lx.eval1("query sceneservice item.N ?")
@@ -14,22 +14,23 @@ n = lx.eval1("query sceneservice item.N ?")
 locator=lx.eval('query sceneservice selection ? locator')
 
 # set destination file and folder
-folder = "/home/ben/Documents/tests/modo/feather/renders/bake/02/"
+folder = "W:/RTS/People/Btomad/_RTS_test/richardOld/renders/turntable/v008/bake/"
 strFileName = folder + "body_"
 
-#Move to the first frame/intialize
+# move to the first frame/intialize
 lx.eval("time.step frame first")
 
 
 # bake the object to render output
 for bakePerFrame in range(firstFrame, lastFrame):
 	fileOutput = strFileName + str(counter).zfill(4)
-	# lx.out(fileOutput)
-	lx.eval('bake.obj filename:%s cage:{} dist:0.0' %fileOutput)
 
 	# increment Y rotation on turntable locator
 	lx.command("select.item",item=locator)
 	lx.eval('transform.channel rot.Y %s.0' %counterRot)
+
+	# lx.out(fileOutput)
+	lx.eval('bake.obj filename:%s cage:{} dist:0.0' %fileOutput)
 	
 	counterRot+=3
 	counter+=1
