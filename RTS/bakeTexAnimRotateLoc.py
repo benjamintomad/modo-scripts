877afe3ade9651df3f701da243b6c2bc62cf1e6f
ben <pixelsnatcher>
2015-03-21 14:57:14 +0100

    auto indent lines

--- RTS/bakeTexAnimRotateLoc.py
+++ RTS/bakeTexAnimRotateLoc.py
@@ -12,7 +12,7 @@ counterRot = firstFrame * 3
 
 #get the name of the character
 sceneName = lx.eval("query sceneservice scene.name ? current")
-scenePart = sceneName.split ('_') 
+scenePart = sceneName.split ('_')
 character = scenePart[0]
 
 
@@ -21,9 +21,9 @@ n = lx.eval1("query sceneservice item.N ?")
 
 # select the render output
 for i in range(n):
-	itemType = lx.eval("query sceneservice item.type ? %s" % i)
-	if(itemType == "renderOutput"):
-		lx.eval('select.itemPattern bake')
+    itemType = lx.eval("query sceneservice item.type ? %s" % i)
+    if(itemType == "renderOutput"):
+        lx.eval('select.itemPattern bake')
 
 # select the uv map
 lx.eval('select.vertexMap pack txuv replace')
@@ -43,17 +43,17 @@ lx.eval("time.step frame first")
 
 # bake the object to render output
 for bakePerFrame in range(firstFrame, lastFrame):
-	fileOutput = strFileName + str(counter).zfill(4)
+    fileOutput = strFileName + str(counter).zfill(4)
 
-	# increment Y rotation on turntable locator
-	lx.command("select.item",item=locator)
-	lx.eval('transform.channel rot.Y %s.0' %counterRot)
+    # increment Y rotation on turntable locator
+    lx.command("select.item",item=locator)
+    lx.eval('transform.channel rot.Y %s.0' %counterRot)
 
-	# lx.out(fileOutput)
-	lx.eval('bake.obj filename:%s format:openexr cage:{} dist:0.0' %fileOutput)
-	
-	counterRot+=3
-	counter+=1
+    # lx.out(fileOutput)
+    lx.eval('bake.obj filename:%s format:openexr cage:{} dist:0.0' %fileOutput)
+
+    counterRot+=3
+    counter+=1
 
 # reset the locator rotation
 lx.command("select.item",item=locator)
