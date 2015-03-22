e139c7cfef857bfcd611f966a3e3a8043e5a6832
ben <pixelsnatcher>
2015-03-22 18:04:36 +0100

    basic interface for comparing preview renders

---
+++ KITS/wtd_comparePreviews/pyscripts/initCompare.py
@@ -0,0 +1,36 @@
+# python
+
+import lx
+from pyModo import pyModo as pym
+
+
+# search if temp folder already exists
+pym.Item_DeSelect()
+allItems = pym.Scene_Get_Item_IDs_All()
+for i in allItems:
+    itemType = pym.Item_Type_Get(i)[0]
+    if itemType == 'imageFolder':
+        pym.Item_Select(i)
+        itemName = pym.Item_Name_Get(i)
+        if itemName == 'tmpPreviews':
+            folderExists = 1
+    else:
+        folderExists = 0
+
+
+
+# creates the temp folder
+def initcompare():
+    lx.eval('clip.newFolder')
+    lx.eval('clip.name tmpPreviews')
+
+
+# dialog error or creates folder
+if folderExists == 1:
+    lx.eval('dialog.title "Initialize"')
+    lx.eval('dialog.msg "Already initialized"')
+    lx.eval('dialog.open')
+else:
+    initcompare()
+
+
---
+++ KITS/wtd_comparePreviews/pyscripts/storePreview02.py
@@ -0,0 +1,23 @@
+# python
+
+from pyModo import pyModo as pym
+import lx
+
+
+
+# allItems = pym.Scene_Get_Item_IDs_All()
+# for i in allItems:
+#     lx.out(i)
+
+
+path = '/home/ben/Documents/tests/modo/comparePreviews/storedImages/preview02.exr'
+
+def storepreview(path):
+    lx.eval('iview.saveImage filename:%s' % path)
+    lx.eval('select.itemPattern tmpPreviews')
+    folderID = pym.Item_ID_Get('tmpPreviews')
+    lx.eval('clip.addStill %s' % path)
+    lx.eval('clip.replace filename:%s type:videoStill' % path)
+    lx.eval('item.parent {preview02:videoStill001} %s 0' % folderID)
+storepreview(path)
+
