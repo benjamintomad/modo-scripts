2f96d210c16da5b1b54bb9785d96dcbcf8018dcc
ben <pixelsnatcher>
2015-03-22 14:36:50 +0100

    if no bake, add it to the scene

--- KITS/wtd_shadingTools/pyscripts/loadBake.py
+++ KITS/wtd_shadingTools/pyscripts/loadBake.py
@@ -13,24 +13,51 @@ character = sceneFile[0]
 filename = 'body_0000.exr'
 folder = '/home/ben/Documents/tests/modo/prepareShadingScene/renders/sha/%s/modo/bake/' % character
 
-
-# get the bake sequence
-allItems = pym.Scene_Get_Item_IDs_All()
-for i in allItems:
-    typ = pym.Item_Type_Get(i)[0]
-    if typ == 'videoSequence':
-        videoName = pym.Item_Name_Get(i)
-        if re.match('body', videoName):
-            bakeSeq = i
-
 # the version set by user
 userdefvalue ='bakeversion'
 getVersion = lx.eval("user.value %s ?" % userdefvalue)
 
-# change the path with new version
-newPath = os.path.join(folder, getVersion, filename)
+# get the version of the currently used bake
+def getcurrentversion():
+    allItems = pym.Scene_Get_Item_IDs_All()
+    for i in allItems:
+        typ = pym.Item_Type_Get(i)[0]
+        if typ == 'videoSequence':
+            videoName = pym.Item_Name_Get(i)
+            if re.match('body', videoName):
+                pym.Item_Select(i)
+                currentFile = pym.Item_Channel_Get_Value('pattern')
+                splitFile = currentFile.split('/')
+                for s in splitFile:
+                    if re.match("v([0-9][0-9][0-9])", s, re.IGNORECASE):
+                        return s
+currentVersion = getcurrentversion()
 
-# replace the clip
-pym.Item_DeSelect()
-pym.Item_Select(bakeSeq)
-lx.eval('clip.replace filename:%s type:videoSequence' % newPath)
+# if no bake in the scene load the version from user
+if currentVersion == None:
+    path = os.path.join(folder, getVersion, filename)
+    lx.eval('clip.addSequence %s' % path)
+
+
+# replace the bake with user value
+else:
+    # get the bake sequence
+    allItems = pym.Scene_Get_Item_IDs_All()
+    for i in allItems:
+        typ = pym.Item_Type_Get(i)[0]
+        if typ == 'videoSequence':
+            videoName = pym.Item_Name_Get(i)
+            if re.match('body', videoName):
+                bakeSeq = i
+
+    # change the path with new version
+    newPath = os.path.join(folder, getVersion, filename)
+
+    # replace the clip
+    pym.Item_DeSelect()
+    pym.Item_Select(bakeSeq)
+    lx.eval('clip.replace filename:%s type:videoSequence' % newPath)
+
+
+
+
